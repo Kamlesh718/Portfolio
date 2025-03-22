bfb322e2755e097e9cc847c9f3b083a0477ad230
Kamlesh718 <kamleshvishwakarma1202@gmail.com>
2025-03-22 18:01:57 +0530

    updated portfolio

---
+++ src/sections/Experience/Experience.jsx
@@ -0,0 +1,14 @@
+import styles from "../projects/ProjectsStyles.module.css";
+
+function Experience() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Experience</h1>
+      <h2>Software Developer Intern (FrontEnd)</h2>
+      <h3>Nablasol - Remote</h3>
+      <p>Oct 2024 – Apr 2025</p>
+    </section>
+  );
+}
+
+export default Experience;
